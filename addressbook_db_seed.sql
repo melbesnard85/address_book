0a27e0f3dc51c945e5ed02023de83b9212a2b580
melbesnard85 <melbesnard85@gmail.com>
2022-12-03 02:45:01 +0900

    feat:backup database migration and seed

---
+++ addressbook_db_seed.sql
@@ -0,0 +1,27 @@
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Records of cities
+-- ----------------------------
+INSERT INTO `cities` VALUES (1, 'New York');
+INSERT INTO `cities` VALUES (2, 'Tokyo');
+
+-- ----------------------------
+-- Records of contacts
+-- ----------------------------
+INSERT INTO `contacts` VALUES (12, 'Natali Fia', 'fin', 'natalia@gmail.com', 'sjsien', '12-sjkdjf', 2);
+INSERT INTO `contacts` VALUES (13, 'Mel dfg', 'melwin', 'melbest@gmail.com', 'la', '123485', 1);
+INSERT INTO `contacts` VALUES (14, 'Sabi Ola', 'aki', 'kai@gmai.com', 'skji lsih', '123jsj0', 1);
+INSERT INTO `contacts` VALUES (15, 'Gkei skeu', 'fin', 'natalia@gmail.com', 'sjsien', '12-sjkdjf', 1);
+INSERT INTO `contacts` VALUES (16, 'Mel chai', 'sdge', 'melbest@gmail.com', 'la', '123485', 1);
+INSERT INTO `contacts` VALUES (17, 'Adf Eju', 'aki', 'kai@gmai.com', 'skji lsih', '123jsj0', 1);
+INSERT INTO `contacts` VALUES (18, 'Natali  Eei', 'fin', 'natalia@gmail.com', 'sjsien', '12-sjkdjf', 1);
+INSERT INTO `contacts` VALUES (19, 'Mel chai', 'dcxe', 'melbest@gmail.com', 'la', '123485', 1);
+INSERT INTO `contacts` VALUES (20, 'Keil skie', 'aki', 'kai@gmai.com', 'skji lsih', '123jsj0', 1);
+INSERT INTO `contacts` VALUES (21, 'Natali sd', 'fin', 'natalia@gmail.com', 'sjsien', '12-sjkdjf', 1);
+INSERT INTO `contacts` VALUES (22, 'Mel shbh', 'melwin', 'melbest@gmail.com', 'la', '123485', 1);
+INSERT INTO `contacts` VALUES (56, 'Jdks sjdf', 'ahke', 'sdf@fsdf.com', 'ksjf', 'ksdfj', 1);
+
+SET FOREIGN_KEY_CHECKS = 1;
--- addressbook_migration.sql
+++ addressbook_migration.sql
@@ -11,8 +11,10 @@
  Target Server Version : 100427
  File Encoding         : 65001
 
- Date: 02/12/2022 21:51:35
+ Date: 03/12/2022 02:31:50
 */
+CREATE DATABASE IF NOT EXISTS addressbooks CHARACTER SET utf8mb4;
+USE addressbooks;
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
@@ -28,12 +30,6 @@ CREATE TABLE `cities`  (
 ) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of cities
--- ----------------------------
-INSERT INTO `cities` VALUES (1, 'New York');
-INSERT INTO `cities` VALUES (2, 'Tokyo');
-
--- ----------------------------
 -- Table structure for contacts
 -- ----------------------------
 DROP TABLE IF EXISTS `contacts`;
@@ -47,21 +43,5 @@ CREATE TABLE `contacts`  (
   `city_id` int(12) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 57 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of contacts
--- ----------------------------
-INSERT INTO `contacts` VALUES (12, 'Natali Fia', 'fin', 'natalia@gmail.com', 'sjsien', '12-sjkdjf', 2);
-INSERT INTO `contacts` VALUES (13, 'Mel dfg', 'melwin', 'melbest@gmail.com', 'la', '123485', 1);
-INSERT INTO `contacts` VALUES (14, 'Sabi Ola', 'aki', 'kai@gmai.com', 'skji lsih', '123jsj0', 1);
-INSERT INTO `contacts` VALUES (15, 'Gkei skeu', 'fin', 'natalia@gmail.com', 'sjsien', '12-sjkdjf', 1);
-INSERT INTO `contacts` VALUES (16, 'Mel chai', 'sdge', 'melbest@gmail.com', 'la', '123485', 1);
-INSERT INTO `contacts` VALUES (17, 'Adf Eju', 'aki', 'kai@gmai.com', 'skji lsih', '123jsj0', 1);
-INSERT INTO `contacts` VALUES (18, 'Natali  Eei', 'fin', 'natalia@gmail.com', 'sjsien', '12-sjkdjf', 1);
-INSERT INTO `contacts` VALUES (19, 'Mel chai', 'dcxe', 'melbest@gmail.com', 'la', '123485', 1);
-INSERT INTO `contacts` VALUES (20, 'Keil skie', 'aki', 'kai@gmai.com', 'skji lsih', '123jsj0', 1);
-INSERT INTO `contacts` VALUES (21, 'Natali sd', 'fin', 'natalia@gmail.com', 'sjsien', '12-sjkdjf', 1);
-INSERT INTO `contacts` VALUES (22, 'Mel shbh', 'melwin', 'melbest@gmail.com', 'la', '123485', 1);
-INSERT INTO `contacts` VALUES (56, 'Jdks sjdf', 'ahke', 'sdf@fsdf.com', 'ksjf', 'ksdfj', 1);
 
 SET FOREIGN_KEY_CHECKS = 1;
