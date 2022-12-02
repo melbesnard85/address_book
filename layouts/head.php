c45da1a774f16b5353c9942f2f35784bc5665904
melbesnard85 <melbesnard85@gmail.com>
2022-12-02 09:04:34 +0900

    reafactor:remove unused code

--- layouts/head.php
+++ layouts/head.php
@@ -18,13 +18,6 @@
         
         <link rel="stylesheet" href="./assets/css/jquery.dataTables.min.css">
         <script src="./assets/js/jquery.dataTables.min.js"></script>
-
-        <script src="https://cdn.datatables.net/buttons/2.3.2/js/dataTables.buttons.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-        <script src="https://cdn.datatables.net/buttons/2.3.2/js/buttons.html5.min.js"></script>
-        <script src="https://cdn.datatables.net/buttons/2.3.2/js/buttons.print.min.js"></script>
         
         <style>
             .wrapper{
